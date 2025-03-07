19367ac5f5f5dd800e356ecc9b4a1541b5a84a66
Hugo Parente Lima <hugo.pl@gmail.com>
2025-03-07 11:38:29 -0300

    Fix compilation with Crystal 1.15.1 by not forking on main.
    Fix #22

--- src/main.cr
+++ src/main.cr
@@ -1,26 +1,3 @@
-{% if flag?(:unix) && flag?(:release) %}
-  # If in release mode we fork and exit before doing anything, this will make possible to
-  # open rtfm from terminals without blocking the terminal.
-  # In debug mode we don't do this, because everybody loves printf debug style
-  fun main(argc : Int32, argv : UInt8**) : Int32
-    # if not using --license or --version, fork and let the terminal free
-    should_fork = true
-    argc.times do |i|
-      if !LibC.strcmp(argv[i], "--version") || !LibC.strcmp(argv[i], "--license")
-        should_fork = false
-        break
-      end
-    end
-
-    if !should_fork || (pid = LibC.fork.zero?)
-      Crystal.main(argc, argv)
-    else
-      LibC.printf("RTFM running 🚀️, you must now be able to RTFM.\n")
-      0
-    end
-  end
-{% end %}
-
 require "colorize"
 require "libadwaita"
 GICrystal.require("WebKit", "6.0")
